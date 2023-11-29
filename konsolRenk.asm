9bb396ee9353ced0ad1d735e3ef0e862d611df4c
bug <147606593+why20w18@users.noreply.github.com>
2023-11-29 14:51:45 +0300

    (10)konsol rengini degistirme islemi

---
+++ konsolRenk.asm
@@ -0,0 +1,29 @@
+org 100h     ;programin ram offsetini 0100 ayarladim
+
+.model small
+.stack 32
+.data
+
+text db 'Welcome bugOS Operating System !','$'
+
+.code 
+      mov ax,@data
+      mov ds,ax
+      
+     
+       
+      ;EKRANA DEGISKENI BASTIR
+      mov dx,offset text    ;text adresini dx'e atadim cagirdigim 9 fonksiyonu dx'e gore hareket eder
+      mov ah,9              ;string bastirmak istiyorum , dx'de stringin ilk degeri olmak zorunda
+      int 21h 
+      
+      
+         ;EKRAN RENGINI DEGISTIR
+        mov ah,0Bh
+        mov al,0Fh
+        int 10h
+         
+          
+     .EXIT
+
+end
