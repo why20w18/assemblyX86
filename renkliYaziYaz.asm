00cfca051cf96891183401462cfe139685073e9f
bug <147606593+why20w18@users.noreply.github.com>
2023-12-02 17:46:38 +0300

    (32)ekranin ortasina renkli string yaz:Macro,push,pop,BIOS kesmesi

--- renkliYaziYaz.asm
+++ renkliYaziYaz.asm
@@ -2,64 +2,59 @@ name "ekranin ortasina mavi arkaplan kirmizi yazi olacak sekilde Assembly X86 ya
 
 .model small
 .stack 32
-.data 
+.data
 
     metin db 'Assembly X86'
-   sutun1 db 14 
+    ;satir hep sabit ama sutun sabit degil
+    sutun1 db 14 ;40/2=20 
+        ;sayi 12
+        ;12/2=6
+        ;20-6=14
     
 .code
-
-    mov ax,@data
-    mov ds,ax
-    mov es,ax
-    mov si,offset metin
-    
-    ;EKRAN ACMA
-    mov ah,0
-    mov al,0 ;40x25
-    int 10h        
-    
-    mov cx,6
-    
-    imlecKonum macro satir,sutun
+        
+        mov ax,@data
+        mov ds,ax
+        mov es,ax
+               
+        mov si,offset metin
+        
+        ;EKRAN AC
+        mov ah,0
+        mov al,0 ; 40x25
+        int 10h
+        
+        mov cx,12
+        
+        imlecKaydir macro satir2 sutun2
         
         mov ah,2 ;IMLEC HIZMETI
-        mov dh,satir
-        mov dl,sutun
-        mov bh,0 ;SAYFA NO
-        int 10h  ;BIOS KESMESI    
+        mov dh,satir2
+        mov dl,sutun2
+        mov bh,0
+        int 10h  ;BIOS KESMESINI CAGIRDIM    
+            
+        imlecKaydir endm
         
-    imlecKonum endm           
-    
-    dongu:   
-    imlecKonum 12,[sutun1]
-    
-    push cx    ;CX DEGERI YUKARDA 6 ASAGIDA TEKRAR YAZDIK 1 DIYE STACKE GONDERIRIM ESKI DEGERI
-    mov ah,9   ;RENKLI YAZMA HIZMETI
-    mov bh,0   ;SAYFA NO
-    mov al,[si];METINDEKI HER KARAKTERIN ADRESI
-    mov cx,1   
-    mov bl,00010100b ;RENK AYARLADIK MAVI ARKAPLAN KIRMIZI YAZI
-    int 10h
-    inc si
-    inc sutun1
-    
-    imlecKonum 12,[sutun1]
-    mov ah,9   
-    mov bh,0   
-    mov al,[si]
-    mov cx,1   
-    mov bl,00010100b
-    int 10h                                                    
-    inc si
-    inc sutun1
-    
-    pop cx
-    loop dongu    
+        dongu:;RENKLI YAZDIRMA ISLEMI BURADA OLACAK
+        push cx
+        imlecKaydir 12,[sutun1]
+        
+        ;RENKLI YAZDIRMA HIZMETI
+        mov ah,9
+        mov bh,0
+        mov cx,1
+        mov bl,00010100b
+        mov al,[si]
+        int 10h
+        inc si       
+        inc sutun1
+        
+        pop cx
+        loop dongu
+        
+        mov ah,04ch
+        int 21h
    
-    
-    
-    
-    mov ah,04ch
-    int 21h
-end    
+   
+end
